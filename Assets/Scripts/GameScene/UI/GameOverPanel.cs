7a6f462efcc42d41d2876dff19e728491b00b651
ryouji <helloryouji@outlook.com>
2024-06-16 16:35:04 +0800

    feat: add game over panel

--- Assets/Scripts/GameScene/UI/GameOverPanel.cs
+++ Assets/Scripts/GameScene/UI/GameOverPanel.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverPanel : BasePanel
 {
-    public Text txtWin;
-    public Text txtInfo;
-    public Text txtMoney;
+    public TMP_Text resultText;
+    public TMP_Text reward;
 
-    public Button btnSure;
+    public Button ensureBtn;
 
     public override void Init()
     {
-        btnSure.onClick.AddListener(()=> {
-            //隐藏面板 
+        ensureBtn.onClick.AddListener(()=> {
+            //隐藏面板
             UIManager.Instance.HidePanel<GameOverPanel>();
             UIManager.Instance.HidePanel<GamePanel>();
             //清空当前关卡的数据
@@ -27,9 +25,8 @@ public class GameOverPanel : BasePanel
 
     public void InitInfo(int money, bool isWin)
     {
-        txtWin.text = isWin ? "通关" : "失败";
-        txtInfo.text = isWin ? "获得胜利奖励" : "获得失败奖励";
-        txtMoney.text = "￥" + money;
+        resultText.text = isWin ? "Win" : "Failure";
+        reward.text = money.ToString();
 
         //根据奖励 改变玩家数据
         GameDataMgr.Instance.playerData.haveMoney += money;
